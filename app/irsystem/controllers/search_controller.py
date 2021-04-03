33f54ae17161d1fa84b45e4499df2ccca4797dcc
madeleinemc <43297249+madeleinemc@users.noreply.github.com>
2021-04-03 17:40:34 -0400

    Add name and netids
    In search_controller.py

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "MyMuseum"
+net_id = "Madeleine Chang: mmc337, Shefali Janorkar: skj28, Esther Lee: esl86, Yvette Hung: yh387, Tiffany Zhong: tz279"
 
 @irsystem.route('/', methods=['GET'])
 def search():
